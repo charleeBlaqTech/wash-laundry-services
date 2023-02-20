03983d6d10ff41bd516f765d22a3aef5a0ed61bf
charles francis <charleeblaq@gmail.com>
2023-02-21 03:10:53 +0530

    modified the about section title header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment,useState } from 'react';
+import React, { Fragment,useState,useRef } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import "./NavBar.css";
 
@@ -17,8 +17,18 @@ const NavBar = () => {
       }
     }
     
-  
-    window.addEventListener("scroll", changeHeaderBg )
+    window.addEventListener("scroll", changeHeaderBg );
+
+
+
+
+    const navUlRef= useRef();
+    const navRef= useRef();
+
+    const handleClick= ()=>{
+      navUlRef.current.classList.toggle("showNavMenu");
+      navRef.current.classList.toggle("showNav")
+    }
 
 
       
@@ -28,18 +38,19 @@ const NavBar = () => {
             <div className="navBar-container">
                 <div className="logo"><Link to="/">WASH<span>Cleaning Service</span></Link></div>
                 <button type="button" class="nav-toggler js-nav-toggler">
-                    <span><i className='fa fa-bars'></i></span>
+                    <span><i className='fa fa-bars' onClick={handleClick}></i></span>
                 </button>
 
                 
-                <nav className="nav js-nav">
-                    <ul>
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/about">About</NavLink></li>
-                        <li><NavLink to="/service">Services</NavLink></li>
-                        <li><NavLink to="/pricing">Pricing</NavLink></li>
-                        <li><NavLink to="/team">Team</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
+                <nav className="nav js-nav" ref={navRef}>
+                    <ul className='nav-ul'   ref={navUlRef}>
+                        <li><NavLink to="/" onClick={handleClick}>Home</NavLink></li>
+                        <li><NavLink to="/about" onClick={handleClick}>About</NavLink></li>
+                        <li><NavLink to="/service" onClick={handleClick}>Services</NavLink></li>
+                        <li><NavLink to="/pricing" onClick={handleClick}>Pricing</NavLink></li>
+                        <li><NavLink to="/team" onClick={handleClick}>Team</NavLink></li>
+                        <li><NavLink to="/contact" onClick={handleClick}>Contact</NavLink></li>
+                        <i className="fas fa-times" onClick={handleClick}></i>
                     </ul>
                 </nav>
             </div>
